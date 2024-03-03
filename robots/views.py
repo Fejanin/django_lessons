71cb6e138518c5e46f58e79d632c32b6c1a5980f
Shvec Sergei <fejanin@mail.ru>
2024-03-03 18:08:00 +0300

    Перенаправления (redirect). Функция reverse.

--- robots/views.py
+++ robots/views.py
@@ -1,7 +1,10 @@
 from django.http import HttpResponse, HttpResponseNotFound, Http404
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.urls import reverse
+
 
 # Create your views here.
+
 def index(request):
     return HttpResponse('Страница о роботах.')
 
@@ -11,8 +14,13 @@ def categories(request, cat_id):
 
 
 def archive(request, year):
+    if year == 1950:
+        uri = reverse('cat_id', args=(1, ))
+        return redirect(uri)
     if year > 2024:  # можно использовать time, получив значение текущего года
         raise Http404()
+    if year < 2000:
+        return redirect('home')  # временное перемещение (302); ('/', permanent=True) ==> постоянное (301)
     return HttpResponse(f'<h1>Архив по годам.</h1><p>{year}</p>')
 
 
