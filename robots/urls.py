673df783804e99a8b288563c8451b68ff2bf8b08
Shvec Sergei <fejanin@mail.ru>
2024-03-03 17:06:52 +0300

    Динамические URL. Пользовательские конвертеры.

--- robots/urls.py
+++ robots/urls.py
@@ -1,7 +1,12 @@
-from django.urls import path
+from django.urls import path, register_converter
 from . import views
+from . import converters
+
+
+register_converter(converters.FourDigitYearConverter, "year4")
 
 urlpatterns = [
     path('', views.index),
-    path('cat/', views.categories),
+    path('cat/<int:cat_id>/', views.categories),
+    path('archive/<year4:year>', views.archive),
 ]
--- robots/views.py
+++ robots/views.py
@@ -6,5 +6,9 @@ def index(request):
     return HttpResponse('Страница о роботах.')
 
 
-def categories(request):
-    return HttpResponse('<h1>Статьи по категориям.</h1>')
+def categories(request, cat_id):
+    return HttpResponse(f'<h1>Статьи по категориям.</h1><p>cat_id: {cat_id}</p>')
+
+
+def archive(request, year):
+    return HttpResponse(f'<h1>Архив по годам.</h1><p>{year}</p>')
