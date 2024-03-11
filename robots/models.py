ee4d2dd5229872c26d7c565473d509f3e7d165f2
Shvec Sergei <fejanin@mail.ru>
2024-03-11 19:32:44 +0300

    Сортировка, изменение и удаление записей.

--- robots/models.py
+++ robots/models.py
@@ -8,3 +8,12 @@ class Robot(models.Model):
     time_update = models.DateTimeField(auto_now=True)
     is_published = models.BooleanField(default=True)
 
+    def __str__(self):
+        return self.title
+
+    class Mets:
+        ordering = ['-time_create']
+        indexes = [
+            models.Index(fields=['-time_create'])
+        ]
+
