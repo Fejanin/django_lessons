c55bd0683e76112e6bf41f34b4878f2026d250b0
Shvec Sergei <fejanin@mail.ru>
2024-03-20 20:20:53 +0300

    Панель поиска и панель фильтрации.

--- robots/admin.py
+++ robots/admin.py
@@ -10,6 +10,8 @@ class RobotAdmin(admin.ModelAdmin):
     list_editable = ('is_published', 'cat',)
     list_per_page = 3
     actions = ['set_published', 'set_draft']
+    search_fields = ['title', 'cat__name']
+    list_filter = ['cat__name', 'is_published']
 
     @admin.display(description='Краткое описание', ordering='content')
     def brief_info(self, robot: Robot):
