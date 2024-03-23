a5b3722b9c46f9b15e90d508facbf3fb44979b17
Shvec Sergei <fejanin@mail.ru>
2024-03-23 16:59:21 +0300

    Отображение полей формы. Сохранение переданных данных в БД.

--- robots/forms.py
+++ robots/forms.py
@@ -3,8 +3,8 @@ from .models import Category
 
 
 class AddPostForm(forms.Form):
-    title = forms.CharField(max_length=255)
-    slug = forms.SlugField(max_length=255)
-    content = forms.CharField(widget=forms.Textarea(), required=False)
-    is_published = forms.BooleanField(required=False)
-    cat = forms.ModelChoiceField(queryset=Category.objects.all())
+    title = forms.CharField(max_length=255, label='Заголовок', widget=forms.TextInput(attrs={'class': 'form-input'}))
+    slug = forms.SlugField(max_length=255, label='URL')
+    content = forms.CharField(widget=forms.Textarea(attrs={'cols': 50, 'rows': 5}), required=False, label='Контент')
+    is_published = forms.BooleanField(required=False, initial=True, label='Публикация')
+    cat = forms.ModelChoiceField(queryset=Category.objects.all(), empty_label='Категория не выбрана', label='Категория')
