88862f39bfc43aaf130421fddacb52f919f654a8
Shvec Sergei <fejanin@mail.ru>
2024-03-27 20:07:55 +0300

    Mixins как способ улучшения программного кода.

--- robots/views.py
+++ robots/views.py
@@ -7,51 +7,17 @@ from django.views.generic import TemplateView, ListView, DetailView, FormView, C
 
 from .forms import AddPostForm, UploadFileForm
 from .models import Robot, Category, TagPost, UploadFiles
-
-menu = [
-    {'title': 'О сайте', 'url_name': 'about'},
-    {'title': 'Добавить статью', 'url_name': 'addpage'},
-    {'title': 'Обратная связь', 'url_name': 'contact'},
-    {'title': 'Войти', 'url_name': 'login'},
-]
-
-# def index(request):
-#     posts = Robot.published.all().select_related('cat')  # оптимизация SQL запросов
-#     data = {
-#         'title': 'Главная страница о роботах.',
-#         'menu': menu,
-#         'posts': posts,
-#         'cat_selected': 0,
-#     }
-#     return render(request, 'robots/index.html', context=data)
+from .utils import DataMixin
 
 
-class RodotHome(ListView):
-    # model = Robot  # отбражает все записи, если нужно использовать не все используй get_queryset
+class RodotHome(DataMixin, ListView):  # миксины добавляются первыми
     template_name = 'robots/index.html'
     context_object_name = 'posts'  # по умолчанию доступ к списку в шаблоне (html) можно получить по object_list
-    extra_context = {
-        'title': 'Главная страница о роботах.',
-        'menu': menu,
-        'cat_selected': 0,
-    }
+    title_page = 'Главная страница'
+    cat_selected = 0
 
     def get_queryset(self):
         return Robot.published.all().select_related('cat')  # оптимизация SQL запросов
-
-    # def get_context_data(self, **kwargs):
-    #     context = super().get_context_data(**kwargs)
-    #     context['title'] = 'Главная страница о роботах.'
-    #     context['menu'] = menu
-    #     context['posts'] = Robot.published.all().select_related('cat')  # оптимизация SQL запросов
-    #     context['cat_selected'] = int(self.request.GET.get('cat_id', 0))
-    #     return context
-
-
-        # def handle_uploaded_file(f):
-#     with open(f'upload/{f.name}', 'wb+') as destination:
-#         for chunk in f.chunks():
-#             destination.write(chunk)
 
 
 def about(request):
@@ -65,106 +31,37 @@ def about(request):
         form = UploadFileForm()
     data = {
         'title': 'О нас',
-        'menu': menu,
+        # 'menu': menu,
         'form': form,
     }
     return render(request, 'robots/about.html', context=data)
 
 
-# def show_post(request, post_slug):
-#     post = get_object_or_404(Robot, slug=post_slug)
-#     data = {
-#         'title': 'Главная страница о роботах.',
-#         'menu': menu,
-#         'post': post,
-#         'cat_selected': Robot.objects.get(slug=request.path.split('/')[-2]).cat_id,
-#     }
-#     return render(request, 'robots/post.html', context=data)
-
-
-class ShowPost(DetailView):
-    # model = Robot
+class ShowPost(DataMixin, DetailView):  # миксины добавляются первыми
     template_name = 'robots/post.html'
     slug_url_kwarg = 'post_slug'
     context_object_name = 'post'
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        context['title'] = context['post'].title
-        context['menu'] = menu
-        return context
+        return self.get_mixin_context(context, title=context['post'].title)
 
     def get_object(self, queryset=None):
         return get_object_or_404(Robot.published, slug=self.kwargs[self.slug_url_kwarg])
 
 
-# def addpage(request):
-#     if request.method == 'POST':
-#         form = AddPostForm(request.POST, request.FILES)
-#         if form.is_valid():
-#             # print(form.cleaned_data)
-#             # try:
-#             #     Robot.objects.create(**form.cleaned_data)
-#             #     return redirect('home')
-#             # except:
-#             #     form.add_error(None, 'Ошибка добавления поста')
-#             form.save()
-#             return redirect('home')
-#     else:
-#         form = AddPostForm()
-#
-#     data = {
-#         'menu': menu,
-#         'title': 'Добавление статьи',
-#         'form': form,
-#     }
-#     return render(request, 'robots/addpage.html', context=data)
-
-
-class AddPage(CreateView):
+class AddPage(DataMixin, CreateView):
     form_class = AddPostForm
-    # model = Robot  # с классом CreateView
-    # fields = '__all__'  # если нужно отображать не все поля ==> используй список # с классом CreateView
     template_name = 'robots/addpage.html'
-    # success_url = reverse_lazy('home')   # по умолчанию найдет маршрут по ф-ции get_absolute_url ==> произойдет переход на созданную статью
-    extra_context = {
-        'menu': menu,
-        'title': 'Добавление статьи',
-    }
+    title_page = 'Добавление статьи'
 
 
-class UpdatePage(UpdateView):
+class UpdatePage(DataMixin, UpdateView):
     model = Robot
     fields = '__all__'  # если нужно отображать не все поля ==> используй список
     template_name = 'robots/addpage.html'
     success_url = reverse_lazy('home')
-    extra_context = {
-        'menu': menu,
-        'title': 'Редактирование статьи',
-    }
-
-
-# class AddPage(View):
-#     def get(self, request):
-#         form = AddPostForm()
-#         data = {
-#             'menu': menu,
-#             'title': 'Добавление статьи',
-#             'form': form,
-#         }
-#         return render(request, 'robots/addpage.html', context=data)
-#
-#     def post(self, request):
-#         form = AddPostForm(request.POST , request.FILES)
-#         if form.is_valid():
-#             form.save()
-#             return redirect('home')
-#         data = {
-#             'menu': menu,
-#             'title': 'Добавление статьи',
-#             'form': form,
-#         }
-#         return render(request, 'robots/addpage.html', context=data)
+    title_page = 'Редактирование статьи'
 
 
 def contact(request):
@@ -175,18 +72,7 @@ def login(request):
     return HttpResponse('Авторизация.')
 
 
-def show_category(request, cat_slug):
-    category = get_object_or_404(Category, slug=cat_slug)
-    posts = Robot.published.filter(cat_id=category.pk).select_related('cat')  # оптимизация SQL запросов
-    data = {
-        'title': f'Рубрика: {category.name}',
-        'menu': menu,
-        'posts': posts,
-        'cat_selected': category.pk,
-    }
-    return render(request, 'robots/index.html', context=data)
-
-class RobotCategory(ListView):
+class RobotCategory(DataMixin, ListView):
     template_name = 'robots/index.html'
     context_object_name = 'posts'
     allow_empty = False  # если в get_context_data ==> cat получаем пустой список, то сгенерируется ошибка 404
@@ -197,25 +83,14 @@ class RobotCategory(ListView):
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         cat = context['posts'][0].cat
-        context['title'] = 'Категория - ' + cat.name
-        context['menu'] = menu
-        context['cat_selected'] = cat.pk
-        return context
+        return self.get_mixin_context(
+            context,
+            title='Категория - ' + cat.name,
+            cat_selected=cat.pk,
+        )
 
 
-# def show_tag_postlist(request, tag_slug):
-#     tag = get_object_or_404(TagPost, slug=tag_slug)
-#     posts = tag.tags.filter(is_published=Robot.Status.PUBLISHED).select_related('cat')  # оптимизация SQL запросов
-#     data = {
-#         'title': f'Тег: {tag.tag}',
-#         'menu': menu,
-#         'posts': posts,
-#         'cat_selected': None,
-#     }
-#     return render(request, 'robots/index.html', context=data)
-
-
-class TagPostList(ListView):
+class TagPostList(DataMixin, ListView):
     template_name = 'robots/index.html'
     context_object_name = 'posts'
     allow_empty = False
@@ -223,10 +98,10 @@ class TagPostList(ListView):
     def get_context_data(self, *, object_list=None, **kwargs):
         context = super().get_context_data(**kwargs)
         tag = TagPost.objects.get(slug=self.kwargs['tag_slug'])
-        context['title'] = 'Тег: ' + tag.tag
-        context['menu'] = menu
-        context['cat_selected'] = None
-        return context
+        return self.get_mixin_context(
+            context,
+            title='Тег: ' + tag.tag
+        )
 
     def get_queryset(self):
         return Robot.published.filter(tags__slug=self.kwargs['tag_slug']).select_related('cat')
