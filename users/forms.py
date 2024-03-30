d90de28c9e9600a58321f9b938d6a0a314be4153
Shvec Sergei <fejanin@mail.ru>
2024-03-30 20:52:49 +0300

    Авторизация через email. Профайл пользователя.

--- users/forms.py
+++ users/forms.py
@@ -4,7 +4,7 @@ from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
 
 
 class LoginUserForm(AuthenticationForm):
-    username = forms.CharField(label='Логин',
+    username = forms.CharField(label='Логин / email',
                                 widget=forms.TextInput(attrs={
                                     'class': 'form-input'
                                 }))
@@ -50,3 +50,27 @@ class RegisterUserForm(UserCreationForm):
         if get_user_model().objects.filter(email=email).exists():
             raise forms.ValidationError('Пользователь с таким e-mail уже существует.')
         return email
+
+
+class ProfileUserForm(forms.ModelForm):
+    username = forms.CharField(disabled=True, label='Логин',
+                               widget=forms.TextInput())  # ...TextInput(attrs={'class': 'form-input'})
+    email = forms.CharField(disabled=True, label='E-mail',
+                               widget=forms.TextInput())
+
+    class Meta:
+        model = get_user_model()
+        fields = [
+            'username',
+            'email',
+            'first_name',
+            'last_name',
+        ]
+        labels = {
+            'first_name': 'Имя',
+            'last_name': 'Фамилия'
+        }
+        widgets = {
+            'first_name': forms.TextInput(attrs={'class': 'form-input'}),
+            'last_name': forms.TextInput(attrs={'class': 'form-input'})
+        }
