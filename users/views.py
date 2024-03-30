b32b40243c6efa1bf17ae30e40392380368f75e5
Shvec Sergei <fejanin@mail.ru>
2024-03-30 18:00:10 +0300

    Регистрация пользователей через функции представления.

--- users/views.py
+++ users/views.py
@@ -4,7 +4,7 @@ from django.contrib.auth.forms import AuthenticationForm
 from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
 from django.urls import reverse, reverse_lazy
-from .forms import LoginUserForm
+from .forms import LoginUserForm, RegisterUserForm
 
 
 class LoginUser(LoginView):
@@ -15,24 +15,15 @@ class LoginUser(LoginView):
     # def get_success_url(self):  # для перенаправленя, в случае успешной авторизации (когда не определена
     #     return reverse_lazy('home')  # переменная LOGIN_REDIRECT_URL в файле settings.py
 
-# def login_user(request):
-#     if request.method == 'POST':
-#         form = LoginUserForm(request.POST)
-#         if form.is_valid():
-#             cd = form.cleaned_data
-#             user = authenticate(
-#                 request,
-#                 username=cd['username'],
-#                 password=cd['password']
-#             )
-#             if user and user.is_active:
-#                 login(request, user)
-#                 return HttpResponseRedirect(reverse('home'))
-#     else:
-#         form = LoginUserForm()
-#     return render(request, 'users/login.html', {'form': form})
 
-
-# def logout_user(request):
-#     logout(request)
-#     return HttpResponseRedirect(reverse('users:login'))
+def register(request):
+    if request.method == 'POST':
+        form = RegisterUserForm(request.POST)
+        if form.is_valid():
+            user = form.save(commit=False)
+            user.set_password(form.cleaned_data['password'])  # шифрование пароля
+            user.save()
+            return render(request, 'users/register_done.html')
+    else:
+        form = RegisterUserForm()
+    return render(request, 'users/register.html', {'form': form})
